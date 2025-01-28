53c4e20318170119fabfd2f037dee129ecb327f6
Nibras Ammar <nibras.ammar@etudiant-enit.utm.tn>
2025-01-28 13:40:02 +0100

    Finalising NavBar And Adding 3d Eliments

--- @latest/src/components/canvas/Computers.jsx
+++ @latest/src/components/canvas/Computers.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import { Suspense, useEffect, useState} from 'react';
+import { Canvas } from '@react-three/fiber'
 const Computers = () => {
   return (
     <div>Computers</div>
